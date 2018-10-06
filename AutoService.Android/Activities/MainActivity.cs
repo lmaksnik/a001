c1d0f5331346898cc8f7f6ec9302dd6a69a0cb4d
l.maks.nik@gmail.com <maksim@MacBook-Pro-ww.local>
2018-10-06 15:03:09 +0600

    first commit

---
+++ AutoService.Android/Activities/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace AutoService.Droid
+{
+    [Activity(Label = "AutoService", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_layout_main);
+        }
+    }
+}
---
+++ AutoService.Android/Resources/layout/activity_layout_main.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace AutoService.Droid.Resources.layout
+{
+    public class activity_layout_main : View
+    {
+        public activity_layout_main(Context context) :
+            base(context)
+        {
+            Initialize();
+        }
+
+        public activity_layout_main(Context context, IAttributeSet attrs) :
+            base(context, attrs)
+        {
+            Initialize();
+        }
+
+        public activity_layout_main(Context context, IAttributeSet attrs, int defStyle) :
+            base(context, attrs, defStyle)
+        {
+            Initialize();
+        }
+
+        void Initialize()
+        {
+        }
+    }
+}
